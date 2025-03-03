7fcbaf13a6eb6b25853c68cd9fb5dc6cb273dcd2
Asuka <nfsp412@163.com>
2025-03-03 14:35:33 +0800

    08_data_structure 冒泡排序

--- 08_data_structure/BubbleSort.cpp
+++ 08_data_structure/BubbleSort.cpp
@@ -42,10 +42,12 @@ void swap(int &i, int &j)
 
 void BubbleSort(int arr[], int n)
 {
+    bool flag;
     // 外层循环代表一共需要比较的轮数,例如1个数字需要比较0轮,2个数字需要比较1轮,3个数字需要比较2轮,n个数字需要比较n-1轮
 
     for (int i = 1; i <= n - 1; i++)
     {
+        flag = false;
         // 内层循环代表每一轮需要比较的次数,例如一共3个数字,第一轮的内层循环需要比较2次,第二轮的内层循环需要比较1次,n个数字在第m轮需要比较n-m
 
         for (int j = 1; j <= n - i; j++)
@@ -53,8 +55,14 @@ void BubbleSort(int arr[], int n)
             if (arr[j - 1] > arr[j])
             {
                 swap(arr[j - 1], arr[j]);
+                flag = true;
             }
         }
+        if (flag == false)
+        {
+            return;//无论外层循环到哪一轮了,只要内层循环一次交换没有触发过,证明整个数组已经有序,直接退出外层循环
+        }
+        
     }
 }
 
@@ -62,6 +70,10 @@ int main()
 {
     SeqList L;
     InitList(L, 10);
+
+    // 固定
+    int a[10] = {1,2,3,4,5,6,7,8,9,10};
+    memcpy(L.elem, a, sizeof(a));
 
     PrintList(L);
 
